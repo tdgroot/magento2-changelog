26dcb68c0023a45a449eaad79d118b6d40324759
Timon de Groot <timon@mooore.nl>
2020-10-29 10:19:59 +0100

    Add Magento 2.3.5-p2

--- Magento/google-shopping-ads/Setup/UpgradeData.php
+++ Magento/google-shopping-ads/Setup/UpgradeData.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
@@ -7,14 +8,16 @@ declare(strict_types=1);
 
 namespace Magento\GoogleShoppingAds\Setup;
 
+use Magento\Framework\Mview\View;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Framework\App\Cache\Manager as CacheManager;
 
 /**
  * Class for module data upgrades
  */
-class UpgradeData implements \Magento\Framework\Setup\UpgradeDataInterface
+class UpgradeData implements
+    \Magento\Framework\Setup\UpgradeDataInterface,
+    \Magento\Framework\Setup\Patch\PatchVersionInterface
 {
     /**
      * @var \Magento\Eav\Setup\EavSetupFactory
@@ -25,33 +28,24 @@ class UpgradeData implements \Magento\Framework\Setup\UpgradeDataInterface
      * @var \Magento\Framework\Indexer\IndexerRegistry
      */
     private $indexerRegistry;
-
     /**
-     * @var array
+     * @var \Magento\Framework\Mview\ViewInterfaceFactory
      */
-    private $indexerIds;
-
-    /**
-     * @var CacheManager
-     */
-    private $cacheManager;
+    private $viewFactory;
 
     /**
      * @param \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
      * @param \Magento\Framework\Indexer\IndexerRegistry $indexerRegistry
-     * @param array $indexerIds
-     * @param CacheManager $cacheManager
+     * @param \Magento\Framework\Mview\ViewInterfaceFactory $viewFactory
      */
     public function __construct(
         \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory,
         \Magento\Framework\Indexer\IndexerRegistry $indexerRegistry,
-        array $indexerIds,
-        CacheManager $cacheManager
+        \Magento\Framework\Mview\ViewInterfaceFactory $viewFactory
     ) {
         $this->eavSetupFactory = $eavSetupFactory;
         $this->indexerRegistry = $indexerRegistry;
-        $this->indexerIds = $indexerIds;
-        $this->cacheManager = $cacheManager;
+        $this->viewFactory = $viewFactory;
     }
 
     /**
@@ -59,101 +53,40 @@ class UpgradeData implements \Magento\Framework\Setup\UpgradeDataInterface
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '1.1.0', '<')) {
-            $setup->startSetup();
+        if (version_compare($context->getVersion(), '4.0.1', '<')) {
             /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
             $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-            $eavSetup->addAttribute(
-                \Magento\Catalog\Model\Product::ENTITY,
-                'merchant_center_category',
-                [
-                    'wysiwyg_enabled'=> false,
-                    'html_allowed_on_front'=> false,
-                    'used_for_sort_by'=> false,
-                    'filterable'=> false,
-                    'filterable_in_search'=> false,
-                    'used_in_grid'=> false,
-                    'visible_in_grid'=> false,
-                    'filterable_in_grid'=> false,
-                    'position'=> 0,
-                    'apply_to'=> 'simple,downloadable,virtual,bundle,configurable',
-                    'searchable'=> false,
-                    'visible_in_advanced_search'=> false,
-                    'comparable'=> false,
-                    'used_for_promo_rules'=> false,
-                    'visible_on_front'=> false,
-                    'used_in_product_listing'=> false,
-                    'visible'=> false,
-                    'scope'=> 'global',
-                    'input'=> 'select',
-                    'entity_type_id'=> 4,
-                    'required'=> false,
-                    'option'=> ['values' => GoogleCategories::GOOGLE_CATEGORIES],
-                    'user_defined'=> false,
-                    'label'=> 'Google Merchant Center Category',
-                    'type'=> 'int',
-                    'source'=> \Magento\Eav\Model\Entity\Attribute\Source\Table::class,
-                    'unique'=> false,
-                ]
-            );
-            $setup->endSetup();
-        }
-
-        if (version_compare($context->getVersion(), '1.1.1', '<')) {
-            $setup->startSetup();
-            $setup->updateTableRow(
-                'core_config_data',
-                'path',
-                'scconnector/google/verification_configs',
-                'path',
-                'sales_channels/sales_channel_integration/verification_configs'
-            );
-            $setup->updateTableRow(
-                'core_config_data',
-                'path',
-                'scconnector/google/gtag',
-                'path',
-                'sales_channels/sales_channel_integration/gtag'
-            );
-            $setup->endSetup();
-        }
-
-        if (version_compare($context->getVersion(), '1.1.2', '<')) {
-            foreach ($this->indexerIds as $indexerId) {
-                try {
-                    $indexer = $this->indexerRegistry->get($indexerId);
-                    $indexer->setScheduled(true);
-                } catch (\Exception $e) {
-                    //ignore if indexers were removed
-                }
-            }
-        }
-
-        if (version_compare($context->getVersion(), '1.2.0', '<')) {
-            $setup->startSetup();
-            /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
-            $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-            $id = $eavSetup->getAttributeId(
+            $eavSetup->removeAttribute(
                 \Magento\Catalog\Model\Product::ENTITY,
                 'merchant_center_category'
             );
-            $eavSetup->updateAttribute(
-                \Magento\Catalog\Model\Product::ENTITY,
-                $id,
-                'is_user_defined',
-                true
-            );
-            $indexer = $this->indexerRegistry->get('scconnector_google_feed');
-            $indexer->setScheduled(true);
-            $setup->endSetup();
+            $configsToDelete = [
+                'sales_channels/sales_channel_integration/additional_headers',
+                'sales_channels/sales_channel_integration/api_key',
+                'sales_channels/sales_channel_integration/bypass_key_check',
+                'sales_channels/sales_channel_integration/enable_service_logs',
+                'sales_channels/sales_channel_integration/frontend_url',
+                'sales_channels/sales_channel_integration/gateway_path',
+                'sales_channels/sales_channel_integration/gateway_url',
+                'sales_channels/sales_channel_integration/gtag',
+                'sales_channels/sales_channel_integration/service_path',
+                'sales_channels/sales_channel_integration/unique_id',
+                'sales_channels/sales_channel_integration/verification_configs',
+                'scconnector/google/gtag',
+                'scconnector/google/verification_configs',
+            ];
+            foreach ($configsToDelete as $configPath) {
+                $setup->deleteTableRow(
+                    'core_config_data',
+                    'path',
+                    $configPath
+                );
+            }
         }
+    }
 
-        if (version_compare($context->getVersion(), '2.0.0', '>=')) {
-            $setup->startSetup();
-            $cacheType = ['google_product'];
-            $enabledTypes = $this->cacheManager->setEnabled($cacheType, true);
-            $this->cacheManager->clean($enabledTypes);
-            $setup->endSetup();
-        }
+    public static function getVersion()
+    {
+        return '4.0.1';
     }
 }
